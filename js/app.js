40da204943c983c0d2b8ef2da99f6be104f300ca
MattiaBologna <mattiabologna01@gmail.com>
2024-03-06 15:40:12 +0100

    Add: algoritmo stampa multipli

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('ciao')
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+// per i multipli di 3 stampi “Fizz” al posto del numero e
+// per i multipli di 5 stampi “Buzz” al post del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+
+// Scivere un programm che stampi in console i numeri da 1 a 100
+// ciclo for che si ripete 100 volte
+for (let i = 0; i < 100; i++) {
+    // definizione variabile num da stampare
+    let number = i + 1; 
+    // per i multipli di 3 stampare "Fizz" (e non per i multipli di 5)
+    if (number % 3 === 0 && number % 5 !== 0) {
+        console.log('Fizz')
+    } else if (number % 5 === 0 && number % 3 !== 0) {
+        // per i multipli di 5 stampare "Buzz" (e non per i multipli di 3)
+        console.log('Buzz')
+    } else if (number % 3 === 0 && number % 5 === 0) {
+            // per i multipli sia di 3 che di 5 stampare "FizzBuzz"
+            console.log('FizzBuzz')
+    } else {
+        // se il numero non e ne multiplo di 3 ne di 5 => stamparlo 
+        console.log(number)
+    }
+}
